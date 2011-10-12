ab7cfc73bd27b2c020d2895b19fd727939ebd34f
Erik Pukinskis <erik@snowedin.net>
2011-10-11 21:32:33 -0700

    Get users' names

--- web.js
+++ web.js
@@ -2,7 +2,10 @@ var express = require('express');
 var mongoose = require('mongoose');
     mongoose.connect(process.env.MONGOHQ_URL);
 var app = express.createServer(express.logger());
+
 app.use(express.bodyParser());
+app.use(express.cookieParser());
+app.use(express.session({ secret: "jf93aj90daadt34sefj90feaj90aajd90sj9" }));
 
 var Schema = mongoose.Schema
   , ObjectId = Schema.ObjectId;
@@ -15,13 +18,26 @@ var MessageSchema = new Schema({
 
 var Message = mongoose.model('message', MessageSchema);
 
-app.get('/', function(request, response) {
-  Message.find({}, function(err, messages) {
-    if (err) {
-      console(err);
-    }
-    response.render('room.jade', {locals: {messages: messages}});
-  });
+app.get('/', function(req, res) {
+  if (!req.session.name) {
+    res.redirect('/name');  
+  } else {
+    Message.find({}, function(err, messages) {
+      if (err) {
+        console.log(err);
+      }
+      res.render('room.jade', {locals: {messages: messages}});
+    });
+  }
+});
+
+app.get('/name', function(req,res) {
+  res.render('name.jade');
+})
+
+app.post('/name', function(req,res) {
+  req.session.name = req.body.name
+  res.redirect('/');
 });
 
 app.post('/:room', function(request, response) {
