7142520a792beb9d11ed2ed0295846bf9fb32bac
Erik Pukinskis <erik@snowedin.net>
2011-10-11 19:05:57 -0700

    Added mongoose, initialization seems to work

--- web.js
+++ web.js
@@ -1,6 +1,16 @@
 var express = require('express');
-
+var mongoose = require('mongoose');
+    mongoose.connect(process.env.MONGOHQ_URL);
 var app = express.createServer(express.logger());
+
+var Schema = mongoose.Schema
+  , ObjectId = Schema.ObjectId;
+
+var Message = new Schema({
+    author    : String
+  , text      : String
+  , date      : Date
+});
 
 app.get('/', function(request, response) {
   response.send('Cherb!');
